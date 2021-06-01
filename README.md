fae1932b279d1bbb62e9c6cef58049d951fd76e7
Antonia Pettersson <antoniapettersson@iMac-som-tillhor-Antonia.local>
2021-06-01 14:17:16 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Markdown-blog
 Using Node.js, Express, And MongoDB
+
+## npm install 
+
+## npm run dev to start server 
---
+++ models/article.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const marked = require("marked"); // allows us to create markdown & turn into HTML
+const slugify = require("slugify"); // allows us to convert someting to a URLfriendly slug
+const createDomPurify = require("dompurify"); //allows us to render HTML inside Node
+const { JSDOM } = require("jsdom");
+const dompurify = createDomPurify(new JSDOM().window);
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  markdown: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true,
+  },
+});
+
+articleSchema.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
+  }
+
+  next();
+});
+
+module.exports = mongoose.model("Article", articleSchema);
